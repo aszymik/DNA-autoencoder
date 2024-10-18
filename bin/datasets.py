22bd7516676d43df0df095180af51bbd1da68f82
annnie73 <aniaszymik02@gmail.com>
2024-10-18 12:13:07 +0200

    debugging training loop

--- bin/datasets.py
+++ bin/datasets.py
@@ -28,6 +28,7 @@ class SeqDataset(Dataset):
             if not (encoded_seq is None) and len(SR.seq) == seq_len:
                 X = torch.tensor(encoded_seq)
                 print(f'Encoded seq: {X.shape}')
+                print(X)
                 # X = X.unsqueeze(0)  # shape becomes [1, batch_size, seq_len]
                 X = X.reshape(1, *X.size())  # [1, 4, 200]
                 print(f'Encoded seq after: {X.shape}')
