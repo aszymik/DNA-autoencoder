8807c8a0bd5db017235b351baa9e0fe34350fe11
annnie73 <aniaszymik02@gmail.com>
2024-11-19 18:01:10 +0100

    remove debugging parts

--- fasta_preprocessing/split_2000bp_sequences.py
+++ fasta_preprocessing/split_2000bp_sequences.py
@@ -13,29 +13,9 @@ def split_fasta(input_file, output_file):
                 start = i * 200
                 end = start + 200
                 fragment_seq = seq[start:end]
-                fragment_id = f"{transform_header(record.id)}_part{i+1}"
+                fragment_id = f"{record.id}_part{i+1}"
                 fragment_record = f">{fragment_id}\n{fragment_seq}\n"
                 out_fasta.write(fragment_record)
-
-def transform_header(header):
-    # Split the header into parts
-    parts = header.split()
-    if len(parts) < 6:
-        print(parts)
-        return None
-        # raise ValueError("Input string does not contain enough parts to process.")
-    
-    # Extract relevant fields
-    chromosome = parts[0].replace("chr", "")
-    position = parts[1]
-    transcript = parts[4]
-    gb_code = parts[5]
-    snps = parts[6]
-    
-    # Format the new header
-    new_header = f"{chromosome}:{position}_{transcript}_{gb_code}_{snps}"
-    return new_header
-
 
 def main():
     parser = argparse.ArgumentParser(description="Split 2000 bp sequences in a FASTA file into 10 parts of 200 bp.")
@@ -43,10 +23,7 @@ def main():
     parser.add_argument("output_file", help="Path to the output FASTA file.")
     args = parser.parse_args()
     
-    # split_fasta(args.input_file, args.output_file)
-    with open(args.output_file, "w") as out_fasta:
-        for record in SeqIO.parse(args.input_file, "fasta"):
-            transform_header(record.id)
+    split_fasta(args.input_file, args.output_file)
 
 
 if __name__ == "__main__":
