b09fdbd50e0e5bd4ee419492023dc8e4406685c9
annnie73 <aniaszymik02@gmail.com>
2025-02-25 20:13:28 +0100

    change OHE name

--- bin/common.py
+++ bin/common.py
@@ -4,7 +4,7 @@ import math
 import numpy as np
 import random
 from collections import OrderedDict
-from sklearn.preprocessing import OneHotEncoder as Encoder
+from sklearn.preprocessing import OneHotEncoder
 from .models import *
 from scipy.stats import pearsonr
 import torch.nn.functional as F
@@ -76,7 +76,7 @@ RESULTS_COLS = OrderedDict({
 class OHEncoder:
     def __init__(self, categories=np.array(['A', 'C', 'G', 'T']), noise=True, verbose=True):
         self.noise = noise
-        self.encoder = Encoder(sparse_output=False, categories=[categories], handle_unknown='ignore', dtype=np.int8)
+        self.encoder = OneHotEncoder(sparse_output=False, categories=[categories], handle_unknown='ignore', dtype=np.int8)
         self.dictionary = categories
         self.encoder.fit(categories.reshape(-1, 1))
 		
